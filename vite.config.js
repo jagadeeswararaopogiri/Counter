5c781979376c85a99176b4d3b0f67531c1677741
jagadeeswara rao pogiri <pogirijagadeeswararao2005@gmail.com>
2025-12-09 12:17:04 +0530

    modified:   package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base:"/Counter/",
   plugins: [
     tailwindcss(),
   ],
 })
+
+
